ad9ea2bb3c5eeb3c455b0245d662c7f698b14573
Shubham Rathor <rshubham005@users.noreply.github.com>
2021-03-20 17:56:45 +0530

    display functionality done

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-// Taking user input 
+   // Taking user input 
 let arr =[]
-function addtask()
+let task= document.getElementById("todo")
+let list = document.getElementById("task-list")
+function addtask()  
 {
- let task= document.getElementById("todo")
  arr.push(task.value)
+ task.value=""
+ show(arr)
 }
-console.log(arr)
+//display functionality
+function show(array)
+{
+let final_list=[]
+
+for(let i=0;i<array.length;i++)
+{
+    final_list.push(`<div class="col-md-4">
+     <div class="task">
+         <h1 class="task-head">${array[i]}</h1>
+         <button class="task-btn1">&#10004</button>
+         <button class="task-btn2">&#10008</button>
+     </div>
+     </div>`)
+}
+
+list.innerHTML=`${final_list}`
+}
+// Delete functionality
