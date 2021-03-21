5898225dae90b1f4f942963e2e0622623df69279
Shubham Rathor <rshubham005@users.noreply.github.com>
2021-03-21 18:11:30 +0530

    completed

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
    // Taking user input 
 let arr =[]
+let final_list=[]
 let task= document.getElementById("todo")
 let list = document.getElementById("task-list")
 function addtask()  
 {
+if(task.value!="")
  arr.push(task.value)
  task.value=""
  show(arr)
@@ -11,19 +13,40 @@ function addtask()
 //display functionality
 function show(array)
 {
-let final_list=[]
+final_list=[]
 
 for(let i=0;i<array.length;i++)
 {
     final_list.push(`<div class="col-md-4">
-     <div class="task">
+     <div class="task"  id="${i}">
          <h1 class="task-head">${array[i]}</h1>
-         <button class="task-btn1">&#10004</button>
-         <button class="task-btn2">&#10008</button>
+         <button class="task-btn1" onclick="done(${i})" >&#10004</button>
+         <button class="task-btn2" onclick="delete_ele(${i})">&#10008</button>
      </div>
      </div>`)
 }
 
 list.innerHTML=`${final_list}`
 }
-// Delete functionality
+//done functionality
+ function done(id)
+ {
+     let eleid= document.getElementById(id)
+    eleid.style.background = "black"
+    eleid.style.color="white"
+    eleid.style.opacity="0.7"
+ }
+
+ //delete functionality
+
+ function delete_ele(id)
+ {
+    if(id>=-1)
+    {
+       final_list.splice(id,1)
+       arr.splice(id,1) 
+    }
+
+    list.innerHTML=`${final_list}`
+ }
+ 
